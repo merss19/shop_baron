5778a6143196c7b82341d1b48bfa8c5ce0bd8acd
merss19 <merss19@gmail.com>
2016-03-22 09:58:59 +0300

    22-03

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -88,42 +88,24 @@ $(document).ready(function() {
 
 
 }(jQuery));
-var mainslider = $('.sort-products__list');
-mainslider.owlCarousel({
-	loop:true,
-	nav:true,
-	responsive:{
-		0:{
-			items:1
-		},
-		600:{
-			items:3
-		},
-		1000:{
-			items:4
-		}
-	}
+
+
+$(document).ready(function() {
+
+	var owl =$(".sort-products__list");
+
+	owl.owlCarousel({
+		items: 4, //10 items above 1000px browser width
+		navigation : true,
+		navigationText : ["",""]
+/*		itemsDesktop: [1000, 5], //5 items between 1000px and 901px
+		itemsDesktopSmall: [900, 3], // betweem 900px and 601px
+		itemsTablet: [600, 2], //2 items between 600 and 0
+		itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option*/
+	});
+
+
 });
-
-
-/*var lastslider = $('#last');
-lastslider.owlCarousel({
-	loop:true,
-	nav:true,
-	responsive:{
-		0:{
-			items:1
-		},
-		600:{
-			items:3
-		},
-		1000:{
-			items:4
-		}
-	}
-});*/
-
-
 
 
 
